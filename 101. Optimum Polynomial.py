8a27f459c7e50d62a871178fdec9a4e35ea06aad
AleCanzo <alessandro.canzoneri00@gmail.com>
2026-03-12 16:35:45 +0100

    Completato

--- 101. Optimum Polynomial.py
+++ 101. Optimum Polynomial.py
@@ -4,13 +4,14 @@ import numpy as np
 def OP(n):
     x,y = [],[]
     for i in range(1,n):
-        print(i)
         y.append(sum([pow((-1*i),p) for p in range(0,11)]))
         x.append(i)
-    print(x, y)
     return lagrange(x, y)
 
-p = OP(4)
-
-p.c[np.abs(p.c) < 1e-10] = 0
-print(p)
+if __name__=="__main__":
+    s = 0
+    for i in range(2, 12):    
+        p = OP(i)
+        p.c[np.abs(p.c) < 1e-1] = 0
+        s += int(round(p(i)))
+    print(s)
