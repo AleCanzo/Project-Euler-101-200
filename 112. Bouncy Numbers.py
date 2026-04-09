fa5ca8b37b8227fb0e85b0069511dba121ef547e
AleCanzo <alessandro.canzoneri00@gmail.com>
2026-04-09 15:18:03 +0200

    .

--- 112. Bouncy Numbers.py
+++ 112. Bouncy Numbers.py
@@ -1,19 +1,31 @@
 def is_bouncy(n):
     n_str = str(n)
-    bouncy = False
-    not_bouncy = True
+    type_n = None
     for i in range(len(n_str)):
-        
-        
-    return 
+        if i == 0:
+            continue
+        if int(n_str[i]) > int(n_str[i-1]):
+            if type_n == "decresc":
+                return True
+            elif type_n == None:
+                type_n = "cresc"
+        elif int(n_str[i]) < int(n_str[i-1]):
+            if type_n == "cresc":
+                return True
+            elif type_n == None:
+                type_n = "decresc"
+        elif int(n_str[i]) == int(n_str[i-1]):
+            continue
+    return False
+
 
 if __name__ == "__main__":
-    n = 1
+    n = 101
+    num = 0
     num_bouncy = 0
-    Flag = False
-    while Flag == False:
+    while num != 99:
         if is_bouncy(n):
             num_bouncy += 1
-        if (num_bouncy*100)/n == 99:
-            Flag == True
+        num = int((num_bouncy*100)/n)
         n += 1
+    print(n, "ha percentuale pari a", num)
